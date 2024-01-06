b9524a2c023bf93c4ce419ffa0c6dcf84c636346
[saitamakenmin] <[chsh22052@g.nihon-u.ac.jp]>
2024-01-06 22:04:31 +0900

    pokemon1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <Main />
+    </div>
+  ); 
+}
